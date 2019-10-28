5c3d45dbccbb8f6ef28c841d9e3fac13b95452bb
Rohan Kodati <rohankodati@Rohans-MBP-2.hsd1.mi.comcast.net>
2019-10-27 20:26:39 -0400

    Fixed Stats

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -18,22 +18,23 @@ class SettingsScreen extends React.Component {
     super(props);
     this.state = {
       bacList:[],
-      keysList:[]
+      keysList:[],
+      bacValues:''
     }
   }
   componentDidMount(){
     const { inputList } = this.props;  
-    let bacPerList = [],   keys = [];
+    let bacPerList = [],   keys = [], bacVal = [];
     if(inputList){
         // we get the list of Inputs as a Array of Object, 
         // each object contains properties related to the drinks and bac, time 
         // creating a new Array of Object with bac and date properties.    
         const il = inputList.map((v,i) => {
-          return { 'bac': parseFloat(v.inBacPer), 'date':moment(v.timeTicks).format("DD")}
+          return { 'bac': parseFloat(v.inBacPer), 'date':moment(v.timeTicks).format("MM/DD")}
         })
         // Grouping the array of object by date to show in the bar charts
         const gIL = groupBy(il, function(r){      
-          return parseInt(r.date);
+          return r.date;
         })    
         // getting Array of Keys from the grouped 
         keys = Object.keys(gIL);
@@ -47,9 +48,17 @@ class SettingsScreen extends React.Component {
               //p.push({'value': sB, 'label': v})
               return p;
             }, [])
-        }
+        
+        bacVal = keys.reduce((p,v,i)=>{
+          let bclst = gIL[keys[i]].map(v => v.bac);
+          let sumBCLst = sum(bclst);
+          let sB = parseInt(sumBCLst * 100)
+          p.push(""+v+" - "+sB+"%");      
+          return p;
+        }, []);
+  }
     //const lst = gIL[keys[0]].map(v => v.bac);    
-    this.setState({ bacList : (bacPerList ? bacPerList : []), keysList:(keys ? keys : []) });
+    this.setState({ bacList : (bacPerList ? bacPerList.slice(0, 7) : []), bacValues: bacVal.join(";   "), keysList:(keys ? keys : []) });
   }
   static getDerivedStateFromProps(nextProps, state){
     return null;
@@ -58,14 +67,15 @@ class SettingsScreen extends React.Component {
   
   render(){
     const fill = 'rgb(134,65,244)';
-    const { bacList, keysList } = this.state;    
+    const { bacList, keysList, bacValues } = this.state;    
     return(
-      <View style={{  margin: 20 }}>
+      <View style={styles.container}>
         <View>
-          <Text>BAC% on each day for alcohol consumed</Text>
+        <Text style={styles.textDetHead}>BAC% on each day of alcohol consumed</Text>
+          <Text style={styles.textDet} >{ bacValues }</Text>
         </View>
         { ((bacList.length > 0 && keysList.length > 0) ? (
-        <View style={{ height: 200, padding: 20 }}>                  
+        <View style={{flex:1, height: 200, padding: 20 }}>                  
             <BarChart
                     style={{ flex: 1 }}
                     data={bacList}
@@ -91,6 +101,29 @@ SettingsScreen.navigationOptions = {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection:'column',
+    justifyContent:'center',
+    backgroundColor: '#fff',      
+  },
+  parent:{
+    flex: 1,
+    flexDirection:'row',
+    justifyContent:'center',
+  },
+  textDetHead:{
+    margin:20,
+    justifyContent:'center',
+    alignItems:'center',  
+    fontWeight:'bold',  
+    fontSize:18,
+  },
+  textDet:{
+    margin:20,    
+    fontSize:16,  
+    color:'#464444'  
+  },
   subTextBtm:{
     fontSize:12,
     color:'#6d6d6d'
